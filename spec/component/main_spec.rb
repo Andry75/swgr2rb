ce4fb46fb518feb178b329b999aa145236c1f849
tetiana.pavlenko <tetiana.pavlenko@softheme.com>
2020-02-24 15:02:26 +0200

    small changes

--- spec/component/main_spec.rb
+++ spec/component/main_spec.rb
@@ -10,13 +10,5 @@ RSpec.describe Swgr2rb::Main do
 
       expect { main.execute.message }.to raise_error(error.message)
     end
-
-    it 'exists with an error message when invalid opts are passed' do
-      args = ['win-k0tia6ggslb:9071/1.0/swagger', 'test test']
-      main = Swgr2rb::Main.new(args)
-      error = Swgr2rb::Swgr2rbError.new('')
-
-      expect { main.execute.message }.to raise_error(error.message)
-    end
   end
 end
