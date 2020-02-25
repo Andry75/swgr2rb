3bef1d0f0b4a5e544b12dd23194aa88a96ac4312
Margarita Tyshkevich <margarita.tyshkevich@gmail.com>
2020-02-25 15:01:16 +0200

    Add ability to use .json file path instead of Swagger URL

--- spec/component/main_spec.rb
+++ spec/component/main_spec.rb
@@ -2,11 +2,11 @@ require 'rspec'
 require 'swgr2rb'
 
 RSpec.describe Swgr2rb::Main do
-  context'when parsing options' do
+  context 'when parsing options' do
     it 'exists with an error message when URL is not passed' do
       args = ['test']
       main = Swgr2rb::Main.new(args)
-      error = Swgr2rb::Swgr2rbError.new('Swagger URL is required')
+      error = Swgr2rb::Swgr2rbError.new("Provided Swagger URL/file path '#{args.first}' is neither a URL nor a path of an existing JSON file")
 
       expect { main.execute.message }.to raise_error(error.message)
     end
